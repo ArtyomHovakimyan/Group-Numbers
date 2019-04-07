7af4be325fc48efe08047efc1cbdf1cf51806cde
User <artyom.hovakimyan01@gmail.com>
2019-04-07 19:41:16 +0400

    add query and group

---
+++ Mic.Volo.LINQGroup/Mic.Volo.LINQGroup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.LINQGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tvercol = Enumerable.Range(0, 1000);
+            var numgroup = from i in tvercol
+                           group i by i.ToString().Length;
+            foreach (IGrouping<int, int> item in numgroup)
+            {
+                Console.WriteLine($"Group N_{item.Key}");
+                foreach (var t in item)
+                {
+                    Console.Write(t+" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
